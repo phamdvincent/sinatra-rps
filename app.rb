902aeaa2c9c775d52ce76274795af5b7c3ff1d61
Vincent Pham <40014512+phamdvincent@users.noreply.github.com>
2023-10-28 23:09:32 +0000

    Added completed game

--- app.rb
+++ app.rb
@@ -3,5 +3,21 @@ require "sinatra/reloader"
 
 get("/") do
 
+
   erb(:rules)
+end
+
+get("/rock") do
+  @computer_choice = rand(1..3)
+  erb(:rock)
+end
+
+get("/paper") do
+  @computer_choice = rand(1..3)
+  erb(:paper)
+end
+
+get("/scissors") do
+  @computer_choice = rand(1..3)
+  erb(:scissors)
 end
